04ca6dfddc18fc7cad0b4713d43bb4ce49c74ebb
rft0202 <rftester@email.neit.edu>
2023-12-12 13:06:54 -0500

    win screen things

---
+++ Unity2Final/Assets/Scripts/WinScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(0,0,45);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
