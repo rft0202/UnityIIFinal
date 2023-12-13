eb0eb08bf9f82ac947fbeef96ad1da30df6bb18d
rft0202 <rftester@email.neit.edu>
2023-12-13 12:04:11 -0500

    fixed win screen

--- Unity2Final/Assets/Scripts/WinScript.cs
+++ Unity2Final/Assets/Scripts/WinScript.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class WinScript : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0,0,45);
-
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        StartCoroutine(MovePlayer());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator MovePlayer()
+    {
+        yield return new WaitForSeconds(0.05f);
+        transform.position = new Vector3(0, 0, 47);
     }
 }
